3c057a9a6f36ede4f66719fd71a53b7c2ab203b9
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-09-13 20:41:12 +0300

    Initial Commit

---
+++ CalculatorV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorV2
+//
+//  Created by Владислав Сизонов on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
